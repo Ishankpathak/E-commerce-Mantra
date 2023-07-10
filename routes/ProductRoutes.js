26922e19c0cbcb00a0b1f6d5d2bdc7fe16360ae0
Ishankpathak <pathakishank4@gmail.com>
2023-07-10 14:20:07 +0530

    Product CRDU operation

---
+++ routes/ProductRoutes.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/ProductController.js";
+import formidable from "express-formidable";
+const router = express.Router();
+
+//route
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//Update Product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get product
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/product/:pid", deleteProductController);
+
+export default router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoute from "./routes/CategoryRoutes.js";
+import productRoutes from "./routes/ProductRoutes.js";
 import cors from "cors";
 
 //config env
@@ -24,6 +25,7 @@ app.use(morgan("dev")); // to see api request in console
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
